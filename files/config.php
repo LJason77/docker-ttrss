e019b2351ee0fb28de440361182522d0a6d58114
LJason <ljason@ljason.cn>
2021-03-01 16:08:08 +0800

    修复错别字和更新

--- files/config.php
+++ files/config.php
@@ -1,9 +1,9 @@
 <?php
-	putenv('TTRSS_SELF_URL_PATH=http://127.0.0.1:3000/');
+	putenv('TTRSS_SELF_URL_PATH=http://192.168.3.102:3000/');
 	putenv('TTRSS_DB_HOST=postgres');
 	putenv('TTRSS_DB_USER=ttrss');
 	putenv('TTRSS_DB_NAME=ttrss');
 	putenv('TTRSS_DB_PASS=ttrss');
 	putenv('TTRSS_SINGLE_USER_MODE=true');
 
-	// putenv('TTRSS_HTTP_PROXY=192.168.1.1:8123');
+	// putenv('TTRSS_HTTP_PROXY=192.168.3.102:8123');
